7ac367c53499edbec80b72c54ac5ef4695e7154c
Martin Trubíni <martin.trubini@hypertext.sk>
2025-11-11 12:53:35 +0100

    Analytics and tracking

--- wp-content/themes/tazilla/assets/js/script.js
+++ wp-content/themes/tazilla/assets/js/script.js
@@ -9,15 +9,15 @@ window.addEventListener("scroll", () => {
         document.body.classList.toggle("scrolled", scrolled);
         isScrolled = scrolled;
     }
-}, { passive: true });
+}, {passive: true});
 
 /**
  * Sticky Features navigation
  */
 document.addEventListener("DOMContentLoaded", () => {
-    const header1 = document.querySelector('body > div.wp-site-blocks > header:nth-child(1)');
-    const header2 = document.querySelector('body > div.wp-site-blocks > header:nth-child(2)');
-    const featuresNavigation = document.querySelector('.tazilla-features-navigation-wrapper');
+    const header1 = document.querySelector("body > div.wp-site-blocks > header:nth-child(1)");
+    const header2 = document.querySelector("body > div.wp-site-blocks > header:nth-child(2)");
+    const featuresNavigation = document.querySelector(".tazilla-features-navigation-wrapper");
 
     if (!header1 || !header2 || !featuresNavigation) return;
 
@@ -37,6 +37,41 @@ document.addEventListener("DOMContentLoaded", () => {
                 featuresNavigation.style.top = `${offset}px`;
             }
         },
-        { passive: true }
+        {passive: true}
     );
+});
+
+/**
+ * Analytics and tracking
+ */
+(function () {
+    // Track when the pricing table becomes visible in the viewport
+    const table = document.querySelector(".tazilla-pricing-table");
+    if (!table) return;
+
+    let hasTracked = false;
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        for (const entry of entries) {
+            if (entry.isIntersecting && !hasTracked) {
+                hasTracked = true;
+
+                dataLayer.push({"event": "pricing_table_viewed"});
+
+                observer.disconnect(); // Stop observing after the first trigger
+            }
+        }
+    }, {
+        root: null,    // viewport
+        threshold: 0.3 // element at least 30% visible
+    });
+
+    observer.observe(table);
+})();
+
+// Track when the "Try for free" button is clicked
+document.querySelectorAll(".wp-block-button[data-try-for-free] a, .tazilla-pricing-table-header-cell .wp-block-button a").forEach((button) => {
+    button.addEventListener("click", () => {
+        dataLayer.push({"event": "try_free_button_clicked"});
+    });
 });
